cfbd5f6c5ecd30a00c14ee7712819b2076087edc
Christian <c.peters@makermaker.de>
2022-08-30 11:53:41 +0200

    Erste Component gebaut

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <p>LOGO</p>
+            <p>Menü</p>
+        </nav>
+    )
+}
+
+export default Navbar
